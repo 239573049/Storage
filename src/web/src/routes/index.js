1aad4d1305ede48ced5862032e023cd67378791f
token <239573049@qq.com>
2022-12-16 18:24:32 +0800

    修改

---
+++ src/web/src/routes/index.js
@@ -0,0 +1,35 @@
+// import {
+//     useRoutes
+// } from 'react-router-dom';
+
+// import Index from '../pages';
+// import Home from '../pages/home';
+// import About from '../pages/about';
+// import Setting from '../pages/setting';
+
+// const Routes = () => {
+//     const routes = useRoutes([
+//         {
+//             path: "/",
+//             element: <Index />,
+//             children: [
+//                 {
+//                     index: true,
+//                     element: <Home />
+//                 },
+//                 {
+//                     path: '/about',
+//                     element: <About />
+//                 },
+//                 {
+//                     path: '/setting',
+//                     element: <Setting />
+//                 }
+//             ]
+//         }
+//     ]);
+
+//     return routes;
+// }
+
+// export default Routes
