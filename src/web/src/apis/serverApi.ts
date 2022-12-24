d8ca0a9d25e44480cd17a5627863c4826419489a
token <239573049@qq.com>
2022-12-24 23:19:38 +0800

    将ui完全切换到bs，控制将在bs界面操作

---
+++ src/web/src/apis/serverApi.ts
@@ -0,0 +1,26 @@
+import axios from 'axios'
+import { StorageDokan } from '../module/storage'
+
+class ServerApi {
+    getlist() {
+        return axios.get("/api/server/list")
+    }
+
+    get(dokan: StorageDokan) {
+        return axios.get("/api/server?dokan=" + dokan)
+    }
+
+    start(dokan: StorageDokan) {
+        return axios.post('/api/server/start?dokan=' + dokan)
+    }
+
+    stop(dokan: StorageDokan) {
+        return axios.post('/api/server/stop?dokan=' + dokan)
+    }
+
+    windowServer() {
+        return axios.post('/api/server/window-server')
+    }
+}
+
+export default new ServerApi
