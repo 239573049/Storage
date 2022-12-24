0ca169675b64c8b01633256e242caa49d933b682
token <239573049@qq.com>
2022-12-24 23:52:38 +0800

    优化web界面

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -9,7 +9,10 @@ import Head from './components/head';
 import Home from './pages/home';
 import axios from 'axios'
 
-axios.defaults.baseURL = "http://127.0.0.1:30099";
+if (process.env.NODE_ENV === 'development') {
+  axios.defaults.baseURL = "http://127.0.0.1:30099";
+}
+
 axios.defaults.timeout = 30000;
 
 class App extends React.Component {
