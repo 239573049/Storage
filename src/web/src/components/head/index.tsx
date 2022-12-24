6dfdb1de48261a5f5ae4d4a948e603a7b6ccf77a
token <239573049@qq.com>
2022-12-25 00:13:25 +0800

    新增github

--- src/web/src/components/head/index.tsx
+++ src/web/src/components/head/index.tsx
@@ -1,10 +1,14 @@
 import { Component, ReactNode } from 'react'
 import style from './index.less'
 import { Link } from 'react-router-dom';
+import {
+    GithubOutlined
+} from '@ant-design/icons';
 
 class Head extends Component {
     render(): ReactNode {
         return (<div className={style.menu}>
+            <a href='https://github.com/239573049/Storage'><div><GithubOutlined /></div></a>
             <Link to={'/setting'}><div>系统设置</div></Link>
             <Link to={'/about'}><div>关于</div></Link>
             <Link to={'/'}><div >首页</div></Link>
--- src/web/src/pages/about/index.tsx
+++ src/web/src/pages/about/index.tsx
@@ -6,8 +6,8 @@ class About extends Component {
         return (<div>
             <div className={style.title}>关于</div>
             <div className={style.content}>本软件是完全免费并且开源的，开源协议基于MIT欢迎更多爱好者贡献！</div>
-            <div className={style.content}>由<a target="_blank" href="https://github.com/239573049">token</a>起始的开源项目；</div>
-        </div>)
+            <div className={style.content}>由<a href="https://github.com/239573049">token</a>起始的开源项目；</div>
+        </div >)
     }
 }
 
